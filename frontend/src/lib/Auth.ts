2ec0cb127ad90abdd96214adbfe700804bbe12f1
Marcel Panse <marcel.panse@gmail.com>
2025-02-02 15:04:36 +0100

    Title favicon (#25)
    * adjust title / favicon
    
    * fix linting

--- frontend/src/lib/Auth.ts
+++ frontend/src/lib/Auth.ts
@@ -25,7 +25,7 @@ export const getUser = async () => {
     // Logged in
     console.log('user is logged in', user)
     return user
-  } catch (_e) {
+  } catch {
     // Not logged in
     // console.log('user is not logged in', _e)
     return null
