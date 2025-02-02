aef436de4b57b02386eb588340254dac3ea37a67
Marcel Panse <marcel.panse@gmail.com>
2025-02-02 21:25:18 +0100

    Collection db (#28)
    * added simple card tracker
    
    * added card component

--- frontend/src/components/Cards.tsx
+++ frontend/src/components/Cards.tsx
@@ -29,6 +29,7 @@ export const Cards: FC<Props> = ({ user }) => {
     if (ownedCard) {
       console.log('updating', ownedCard)
       ownedCard.amount_owned = Math.max(0, ownedCard.amount_owned + increment)
+      setOwnedCards([...ownedCards])
       await db.updateDocument(DATABASE_ID, COLLECTION_ID, ownedCard.$id, { amount_owned: ownedCard.amount_owned })
       await fetchCollection()
     } else if (!ownedCard && increment > 0) {
@@ -42,56 +43,40 @@ export const Cards: FC<Props> = ({ user }) => {
     }
   }
 
-  //TODO refactor this into a component
+  const Card = ({ cardId }: { cardId: string }) => {
+    return (
+      <div className="flex items-center gap-x-2">
+        Card A01
+        <button
+          type="button"
+          onClick={() => updateCardCount(cardId, -1)}
+          className="cursor-pointer rounded-full bg-indigo-600 p-1 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          <svg className="size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
+            <path d="M5 10a.75.75 0 0 1 .75-.75h8.5a.75.75 0 0 1 0 1.5h-8.5A.75.75 0 0 1 5 10z" />
+          </svg>
+        </button>
+        <span>{ownedCards.find((c) => c.card_id === cardId)?.amount_owned || 0}</span>
+        <button
+          type="button"
+          onClick={() => updateCardCount(cardId, 1)}
+          className="cursor-pointer rounded-full bg-indigo-600 p-1 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          <svg className="size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
+            <path d="M10.75 4.75a.75.75 0 0 0-1.5 0v4.5h-4.5a.75.75 0 0 0 0 1.5h4.5v4.5a.75.75 0 0 0 1.5 0v-4.5h4.5a.75.75 0 0 0 0-1.5h-4.5v-4.5Z" />
+          </svg>
+        </button>
+      </div>
+    )
+  }
+
   return (
     <ul className="mt-8 space-y-2">
       <li>
-        <div className="flex items-center gap-x-2">
-          Card A01
-          <button
-            type="button"
-            onClick={() => updateCardCount('A01', -1)}
-            className="cursor-pointer rounded-full bg-indigo-600 p-1 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            <svg className="size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-              <path d="M5 10a.75.75 0 0 1 .75-.75h8.5a.75.75 0 0 1 0 1.5h-8.5A.75.75 0 0 1 5 10z" />
-            </svg>
-          </button>
-          <span>{ownedCards.find((c) => c.card_id === 'A01')?.amount_owned || 0}</span>
-          <button
-            type="button"
-            onClick={() => updateCardCount('A01', 1)}
-            className="cursor-pointer rounded-full bg-indigo-600 p-1 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            <svg className="size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-              <path d="M10.75 4.75a.75.75 0 0 0-1.5 0v4.5h-4.5a.75.75 0 0 0 0 1.5h4.5v4.5a.75.75 0 0 0 1.5 0v-4.5h4.5a.75.75 0 0 0 0-1.5h-4.5v-4.5Z" />
-            </svg>
-          </button>
-        </div>
+        <Card cardId="A01" />
       </li>
       <li>
-        <div className="flex items-center gap-x-2">
-          Card A02
-          <button
-            type="button"
-            onClick={() => updateCardCount('A02', -1)}
-            className="cursor-pointer rounded-full bg-indigo-600 p-1 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            <svg className="size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-              <path d="M5 10a.75.75 0 0 1 .75-.75h8.5a.75.75 0 0 1 0 1.5h-8.5A.75.75 0 0 1 5 10z" />
-            </svg>
-          </button>
-          <span>{ownedCards.find((c) => c.card_id === 'A02')?.amount_owned || 0}</span>
-          <button
-            type="button"
-            onClick={() => updateCardCount('A02', 1)}
-            className="cursor-pointer rounded-full bg-indigo-600 p-1 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            <svg className="size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-              <path d="M10.75 4.75a.75.75 0 0 0-1.5 0v4.5h-4.5a.75.75 0 0 0 0 1.5h4.5v4.5a.75.75 0 0 0 1.5 0v-4.5h4.5a.75.75 0 0 0 0-1.5h-4.5v-4.5Z" />
-            </svg>
-          </button>
-        </div>
+        <Card cardId="A02" />
       </li>
     </ul>
   )
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -18,7 +18,7 @@ export const Home = () => {
   return (
     <div className="flex flex-col justify-center items-center m-40">
       <h1 className="text-3xl font-bold">TCG Pocket Collection Tracker</h1>
-      <p className="mb-5">Coming soon...</p>
+      <p className="mb-5">Work in progress, check back soon!</p>
       {!user && <Login />}
       {user && (
         <>
