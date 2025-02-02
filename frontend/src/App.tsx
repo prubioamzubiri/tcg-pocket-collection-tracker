2763e67d3cdb48226cd8ba6fd33da2a593e37b69
Ruben Javier Bacho Alarcon <130557846+RubenJavierBachoAlarcon@users.noreply.github.com>
2025-02-02 14:45:05 +0100

    Integrate Tailwind CSS into the project (#21)
    * feat: integrate Tailwind CSS into the project
    
    * Add a greeting header to the main app component using Tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,9 @@ import { Verify } from './pages/Verify.tsx'
 function App() {
   return (
     <>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/verify" element={<Verify />} />
