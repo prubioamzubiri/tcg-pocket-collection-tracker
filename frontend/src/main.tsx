6ffde02a0608fcd2f607760a7fc16c60cc0f0055
Marcel Panse <marcel.panse@gmail.com>
2025-02-02 11:54:20 +0100

    trigger build (#19)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,7 +6,7 @@ import './index.css'
 
 const root = document.getElementById('root')
 if (!root) {
-  throw new Error('No root element found')
+  throw new Error('No root element found!')
 }
 
 createRoot(root).render(
