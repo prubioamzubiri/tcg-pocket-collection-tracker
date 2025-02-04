4b4cfb5c73ea040d66f0ebc61d21acc60aac8fba
Marcel Panse <marcel.panse@gmail.com>
2025-02-04 22:25:05 +0100

    Card layout (#53)
    * now showing all cards with images
    
    * added all packs
    
    * grayscale

--- frontend/src/components/Cards.tsx
+++ frontend/src/components/Cards.tsx
@@ -1,9 +1,17 @@
+import { COLLECTION_ID, DATABASE_ID, getDatabase } from '@/lib/Auth.ts'
 import type { CollectionRow } from '@/types'
 import { ID, type Models } from 'appwrite'
 import { type FC, useEffect, useState } from 'react'
 import A1 from '../../assets/cards/A1.json'
-import { COLLECTION_ID, DATABASE_ID, getDatabase } from '../lib/Auth.ts'
+import A1a from '../../assets/cards/A1a.json'
+import A2 from '../../assets/cards/A2.json'
+import PA from '../../assets/cards/P-A.json'
 import type { Card as CardType } from '../types'
+
+const a1Cards: CardType[] = A1 as unknown as CardType[]
+const a2Cards: CardType[] = A2 as unknown as CardType[]
+const a1aCards: CardType[] = A1a as unknown as CardType[]
+const paCards: CardType[] = PA as unknown as CardType[]
 
 interface Props {
   user: Models.User<Models.Preferences>
@@ -35,7 +43,7 @@ export const Cards: FC<Props> = ({ user }) => {
       await db.updateDocument(DATABASE_ID, COLLECTION_ID, ownedCard.$id, {
         amount_owned: ownedCard.amount_owned,
       })
-      await fetchCollection()
+      // await fetchCollection()
     } else if (!ownedCard && increment > 0) {
       console.log('adding new card', cardId)
       await db.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
@@ -47,27 +55,29 @@ export const Cards: FC<Props> = ({ user }) => {
     }
   }
 
-  const A1_Cards: CardType[] = A1 as unknown as CardType[]
-
-  const Card = ({ cardId }: { cardId: string }) => {
-    const Card_info = A1_Cards.find((c) => c.id === cardId)
+  const Card = ({ card }: { card: CardType }) => {
+    const amountOwned = ownedCards.find((c) => c.card_id === card.id)?.amount_owned || 0
     return (
       <div className="flex flex-col items-center gap-y-4 w-fit border p-4 rounded-lg shadow-md hover:shadow-lg transition duration-200 group">
-        <img className="w-40 rounded-lg object-cover transform group-hover:scale-105 transition duration-200" src={Card_info?.image} alt={Card_info?.name} />
+        <img
+          className={`${amountOwned === 0 ? 'grayscale' : ''} w-40 rounded-lg object-cover transform group-hover:scale-105 transition duration-200`}
+          src={card?.image}
+          alt={card?.name}
+        />
         <div className="flex items-center gap-x-4 mt-2">
           <button
             type="button"
-            onClick={() => updateCardCount(cardId, -1)}
+            onClick={() => updateCardCount(card.id, -1)}
             className="flex items-center justify-center w-8 h-8 rounded-full bg-red-500 text-white hover:bg-red-400 transition duration-200 focus:outline-none"
           >
             <svg className="w-4 h-4" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
               <path d="M5 10a.75.75 0 0 1 .75-.75h8.5a.75.75 0 0 1 0 1.5h-8.5A.75.75 0 0 1 5 10z" />
             </svg>
           </button>
-          <span className="text-lg font-semibold">{ownedCards.find((c) => c.card_id === cardId)?.amount_owned || 0}</span>
+          <span className="text-lg font-semibold">{amountOwned}</span>
           <button
             type="button"
-            onClick={() => updateCardCount(cardId, 1)}
+            onClick={() => updateCardCount(card.id, 1)}
             className="flex items-center justify-center w-8 h-8 rounded-full bg-green-500 text-white hover:bg-green-400 transition duration-200 focus:outline-none"
           >
             <svg className="w-4 h-4" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
@@ -79,13 +89,27 @@ export const Cards: FC<Props> = ({ user }) => {
     )
   }
 
+  const Pack = ({ cards }: { cards: CardType[] }) => {
+    return (
+      <>
+        <h2 className="text-center text-2xl font-bold my-5">{cards[0].set_details}</h2>
+        <ul className="grid grid-cols-2 md:grid-cols-5 gap-4">
+          {cards.map((card) => (
+            <li key={card.id} className="mx-auto">
+              <Card card={card} />
+            </li>
+          ))}
+        </ul>
+      </>
+    )
+  }
+
   return (
-    <ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center">
-      {A1_Cards.map((card) => (
-        <li key={card.id} className="mx-auto">
-          <Card cardId={card.id} />
-        </li>
-      ))}
-    </ul>
+    <>
+      <Pack cards={a1Cards} />
+      <Pack cards={a2Cards} />
+      <Pack cards={a1aCards} />
+      <Pack cards={paCards} />
+    </>
   )
 }
