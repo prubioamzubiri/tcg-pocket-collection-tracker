3f4ab3d9745c5c57bf03ca1712e5375c1106767b
marcel panse <marcel.panse@gmail.com>
2025-03-08 17:49:55 +0100

    added loading spinner

--- frontend/src/components/PokemonCardDetectorComponent.tsx
+++ frontend/src/components/PokemonCardDetectorComponent.tsx
@@ -39,15 +39,19 @@ interface ExtractedCard {
 const PokemonCardDetector: FC<PokemonCardDetectorProps> = ({ onDetectionComplete, modelPath = '/model/model.json' }) => {
   const { user } = use(UserContext)
   const { ownedCards, setOwnedCards } = use(CollectionContext)
+
+  const fileInputRef = useRef<HTMLInputElement>(null)
+
   const [isOpen, setIsOpen] = useState(false)
-  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [isLoadingModel, setIsLoadingModel] = useState<boolean>(false)
   const [isGeneratingHashes, setIsGeneratingHashes] = useState(false)
-  const [progress, setProgress] = useState('0%')
+  const [initProgress, setInitProgress] = useState('0%')
   const [extractedCards, setExtractedCards] = useState<ExtractedCard[]>([])
   const [amount, setAmount] = useState(1)
-  const [isProcessing, setIsProcessing] = useState(false)
   const [showPotentialMatches, setShowPotentialMatches] = useState(false)
-  const fileInputRef = useRef<HTMLInputElement>(null)
+  const [isUploadingImages, setIsUploadingImages] = useState(false)
+  const [isProcessingCardUpdates, setIsProcessingCardUpdates] = useState(false)
+
   const detectorService = PokemonCardDetectorService.getInstance()
 
   useEffect(() => {
@@ -59,13 +63,13 @@ const PokemonCardDetector: FC<PokemonCardDetectorProps> = ({ onDetectionComplete
 
   const initializeModel = async () => {
     try {
-      setIsLoading(true)
+      setIsLoadingModel(true)
       await detectorService.loadModel(modelPath)
       console.log('Model loaded successfully')
     } catch (error) {
       console.error('Error loading model:', error)
     } finally {
-      setIsLoading(false)
+      setIsLoadingModel(false)
     }
   }
 
@@ -123,7 +127,7 @@ const PokemonCardDetector: FC<PokemonCardDetectorProps> = ({ onDetectionComplete
           await new Promise((resolve) => setTimeout(resolve, 0))
 
           const progress = ((Math.min(i + batchSize, totalCards) / totalCards) * 100).toFixed(0)
-          setProgress(`${progress}%`)
+          setInitProgress(`${progress}%`)
         }
 
         await hashStorageService.storeHashes(allHashes)
@@ -223,6 +227,8 @@ const PokemonCardDetector: FC<PokemonCardDetectorProps> = ({ onDetectionComplete
   }
 
   const handleImageUpload = async (event: ChangeEvent<HTMLInputElement>) => {
+    setIsUploadingImages(true)
+
     const files = event.target.files
     if (!files || files.length === 0) return
 
@@ -251,6 +257,8 @@ const PokemonCardDetector: FC<PokemonCardDetectorProps> = ({ onDetectionComplete
       }
     } catch (error) {
       console.error('Error during detection:', error)
+    } finally {
+      setIsUploadingImages(false)
     }
   }
 
@@ -318,7 +326,7 @@ const PokemonCardDetector: FC<PokemonCardDetectorProps> = ({ onDetectionComplete
   }
 
   const handleConfirm = async () => {
-    setIsProcessing(true)
+    setIsProcessingCardUpdates(true)
 
     const cardIds = extractedCards.filter((card) => card.selected && card.matchedCard).map((card) => card.matchedCard?.id)
 
@@ -340,7 +348,7 @@ const PokemonCardDetector: FC<PokemonCardDetectorProps> = ({ onDetectionComplete
       }
     }
 
-    setIsProcessing(false)
+    setIsProcessingCardUpdates(false)
   }
 
   const renderPotentialMatches = (card: ExtractedCard, index: number) => {
@@ -413,6 +421,26 @@ const PokemonCardDetector: FC<PokemonCardDetectorProps> = ({ onDetectionComplete
       </div>
     )
   }
+
+  const Spinner = () => (
+    <svg
+      aria-hidden="true"
+      className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+      viewBox="0 0 100 101"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+        fill="currentColor"
+      />
+      <path
+        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+        fill="currentFill"
+      />
+    </svg>
+  )
+
   return (
     <div className="pokemon-card-detector flex justify-end">
       <Button onClick={() => setIsOpen(true)} variant="ghost">
@@ -426,7 +454,7 @@ const PokemonCardDetector: FC<PokemonCardDetectorProps> = ({ onDetectionComplete
               <DialogTitle>Pokemon card scanner</DialogTitle>
             </DialogHeader>
 
-            {!isLoading && !isGeneratingHashes && extractedCards.length === 0 && (
+            {!isLoadingModel && !isGeneratingHashes && extractedCards.length === 0 && (
               <div
                 className="file-input-container flex flex-col items-center justify-center p-6 border-2 border-dashed rounded-lg cursor-pointer hover:bg-gray-50 dark:hover:bg-gray-900/10"
                 onClick={() => fileInputRef.current?.click()}
@@ -437,40 +465,33 @@ const PokemonCardDetector: FC<PokemonCardDetectorProps> = ({ onDetectionComplete
                     matches, select or deselect detected cards, and choose how much to increment the quantity for selected cards.
                   </p>
                 </AlertDescription>
-                <input type="file" ref={fileInputRef} onChange={handleImageUpload} accept="image/*" multiple disabled={isLoading} className="w-full hidden" />
-                <Button variant="outline" className="mt-2">
-                  Select images
+                <input
+                  type="file"
+                  ref={fileInputRef}
+                  onChange={handleImageUpload}
+                  accept="image/*"
+                  multiple
+                  disabled={isLoadingModel}
+                  className="w-full hidden"
+                />
+                <Button variant="outline" className="mt-2" disabled={isUploadingImages}>
+                  {isUploadingImages && <Spinner />} Select images
                 </Button>
               </div>
             )}
 
-            {(isLoading || isGeneratingHashes) && (
+            {(isLoadingModel || isGeneratingHashes) && (
               <Alert variant="default">
                 <AlertDescription>
                   <div className="flex items-center space-x-2">
-                    <svg
-                      aria-hidden="true"
-                      className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-                      viewBox="0 0 100 101"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                        fill="currentColor"
-                      />
-                      <path
-                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                        fill="currentFill"
-                      />
-                    </svg>
-                    <p>Downloading and loading scan model, please wait... ({progress})</p>
+                    <Spinner />
+                    <p>Downloading and initializing scan model, please wait... ({initProgress})</p>
                   </div>
                 </AlertDescription>
               </Alert>
             )}
 
-            {!isLoading && extractedCards.length > 0 && (
+            {!isLoadingModel && extractedCards.length > 0 && (
               <div>
                 <div className="flex gap-2 justify-between my-4 flex-wrap">
                   <Button variant="outline" onClick={handleDeselectAll} className="hidden sm:block">
@@ -513,7 +534,7 @@ const PokemonCardDetector: FC<PokemonCardDetectorProps> = ({ onDetectionComplete
               </div>
             )}
 
-            <DialogFooter>
+            <DialogFooter className="gap-y-4">
               <Button
                 variant="outline"
                 onClick={() => {
@@ -528,8 +549,8 @@ const PokemonCardDetector: FC<PokemonCardDetectorProps> = ({ onDetectionComplete
                 {extractedCards.length > 0 ? 'Cancel' : 'Close'}
               </Button>
               {extractedCards.length > 0 && (
-                <Button onClick={handleConfirm} disabled={selectedCount === 0 || isProcessing || isLoading} variant="default">
-                  {isProcessing ? (
+                <Button onClick={handleConfirm} disabled={selectedCount === 0 || isProcessingCardUpdates || isLoadingModel} variant="default">
+                  {isProcessingCardUpdates ? (
                     <svg
                       aria-hidden="true"
                       className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
