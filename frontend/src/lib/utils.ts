81c62daa7778eeb310dd11dba0b9cab46d0d1be4
Mathys PIERREZ <mathyspierrez2@gmail.com>
2025-06-02 08:36:02 +0200

    feat: add A3a cards for differents languages + A3a translations (#440)

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -2,6 +2,8 @@ import type { Card } from '@/types'
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 import pokemonTranslations from '../../assets/pokemon_translations.json'
+import toolTranslations from '../../assets/tools_translations.json'
+import trainerTranslations from '../../assets/trainers_translations.json'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -10,14 +12,33 @@ export function cn(...inputs: ClassValue[]) {
 export function getCardNameByLang(card: Card, lang: string): string {
   if (card.name === undefined || card.name === null) return ''
 
-  let cardName = card.ex === 'yes' ? card.name.slice(0, -3) : card.name
+  switch (card.card_type) {
+    case 'pokémon': {
+      let cardName = card.ex === 'yes' ? card.name.slice(0, -3) : card.name
+      const key = cardName.toLowerCase()
+      const cardNameTranslations = pokemonTranslations[key as keyof typeof pokemonTranslations]
 
-  const key = cardName.toLowerCase()
-  const cardNameTranslations = pokemonTranslations[key as keyof typeof pokemonTranslations]
+      if (cardNameTranslations) {
+        cardName = cardNameTranslations[lang as keyof typeof cardNameTranslations] || cardName
+      }
 
-  if (cardNameTranslations) {
-    cardName = cardNameTranslations[lang as keyof typeof cardNameTranslations] || cardName
+      return card.ex === 'yes' ? `${cardName} ex` : cardName
+    }
+    case 'trainer': {
+      if (card.evolution_type === 'item' || card.evolution_type === 'tool') {
+        const toolNameTranslations = toolTranslations[card.name.toLowerCase() as keyof typeof toolTranslations]
+        if (toolNameTranslations) {
+          return toolNameTranslations[lang as keyof typeof toolNameTranslations] || card.name
+        }
+      } else if (card.evolution_type === 'supporter') {
+        const trainerNameTranslations = trainerTranslations[card.name.toLowerCase() as keyof typeof trainerTranslations]
+        if (trainerNameTranslations) {
+          return trainerNameTranslations[lang as keyof typeof trainerNameTranslations] || card.name
+        }
+      }
+      break
+    }
   }
 
-  return card.ex === 'yes' ? `${cardName} ex` : cardName
+  return card.name
 }
