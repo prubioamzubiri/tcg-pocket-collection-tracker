9cfef693dd1716abf26286d4de47443ddbe46a18
marcel panse <marcel.panse@gmail.com>
2025-07-30 19:36:30 +0200

    A4 missions

--- scripts/scrape-missions.js
+++ scripts/scrape-missions.js
@@ -4,7 +4,7 @@ import * as cheerio from 'cheerio'
 import fetch from 'node-fetch'
 
 const BASE_URL = 'https://bulbapedia.bulbagarden.net/wiki'
-const targetDir = '../frontend/assets/themed-collections/'
+const targetDir = './frontend/assets/themed-collections/'
 const expansions = ['A4']
 // const expansions = ['A1', 'A1a', 'A2', 'A2a', 'A2b', 'A3', 'A3a', 'A3b', 'A4']
 const expansionToName = {
