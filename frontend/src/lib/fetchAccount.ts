b155701b7c0609572145574934262fb89325b54d
marcel panse <marcel.panse@gmail.com>
2025-08-30 08:09:26 +0200

    fix: card collection didnt load when no account row was yet created

--- frontend/src/lib/fetchAccount.ts
+++ frontend/src/lib/fetchAccount.ts
@@ -13,7 +13,15 @@ export async function fetchAccount(email: string) {
   if (data.length > 0) {
     return data[0] as AccountRow
   }
-  return null
+
+  // we don't have an account row in the database, so we have to return some default values.
+  // these are needed for the cache to upsert a new account row
+  return {
+    email,
+    username: '',
+    min_number_of_cards_to_keep: 1,
+    max_number_of_cards_wanted: 1,
+  } as AccountRow & { email: string }
 }
 
 export async function fetchPublicAccount(friendId: string) {
